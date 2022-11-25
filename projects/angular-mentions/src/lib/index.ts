abfdcd528e8f16a822f65cf97d8339f3cb44fac4
Luca Peruzzo <luca.peruzzo@sharry.land>
2022-11-25 11:55:30 +0100

    export fix

--- projects/angular-mentions/src/lib/index.ts
+++ projects/angular-mentions/src/lib/index.ts
@@ -1,5 +1,6 @@
 export * from './caret-coords';
 export * from './mention-utils';
 export * from './mention-list.component';
+export * from './mention.service';
 export * from './mention.directive';
 export * from './mention.module';
